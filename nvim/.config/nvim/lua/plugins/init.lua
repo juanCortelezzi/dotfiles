8164a57a4225f578637532fd068ba1d9f6714be2
J.B.C <juanbicortelezzi@gmail.com>
2025-08-01 12:02:49 +1200

    feat(nvim): add colorizer

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,6 +13,31 @@ return {
     opts = {},
   },
   {
+    "catgoose/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        css = true,
+        tailwind = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>bca",
+        function()
+          require("colorizer").attach_to_buffer()
+        end,
+        desc = "Attach Colorizer to Buffer",
+      },
+      {
+        "<leader>bcd",
+        function()
+          require("colorizer").detach_from_buffer()
+        end,
+        desc = "Detach Colorizer to Buffer",
+      },
+    },
+  },
+  {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
     init = function()
