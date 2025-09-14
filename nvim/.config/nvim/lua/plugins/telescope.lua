d4636db3dc829c631b9eacd6eeb0b3e17f9daad2
J.B.C <juanbicortelezzi@gmail.com>
2025-09-14 19:30:24 +1200

    feat(nvim): rework telescope multigrep to use find

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,50 +3,77 @@ local live_multigrep = function(opts)
   local finders = require("telescope.finders")
   local make_entry = require("telescope.make_entry")
   local conf = require("telescope.config").values
+  local job = require("plenary.job")
 
   opts = opts or {}
   opts.cwd = opts.cwd or vim.uv.cwd()
 
+  local get_fd_pahts_sync = function(pattern)
+    local fd_job = job:new({
+      command = "fd",
+      args = { "-c=never", pattern },
+      cwd = opts.cwd,
+      enable_recording = true,
+    })
+
+    local fd_paths, fd_status_code = fd_job:sync()
+    if fd_status_code > 0 then
+      local err = fd_job:stderr_result()
+      local err_string = ""
+      for index, line in ipairs(err) do
+        if index ~= 1 then
+          err_string = err_string .. " "
+        end
+        err_string = err_string .. line
+      end
+      error(err_string)
+    end
+
+    return fd_paths or {}
+  end
+
+  local command_generator = function(prompt)
+    if not prompt or prompt == "" then
+      return nil
+    end
+
+    local pieces = vim.split(prompt, "  ")
+    local rg_pattern = pieces[1]
+    local fd_pattern = pieces[2]
+
+    local args = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    }
+    if rg_pattern and rg_pattern ~= "" then
+      table.insert(args, "-e")
+      table.insert(args, rg_pattern)
+    end
+
+    if fd_pattern and fd_pattern ~= "" then
+      local fd_paths = get_fd_pahts_sync(fd_pattern)
+      for _, path in ipairs(fd_paths) do
+        table.insert(args, path)
+      end
+    end
+
+    return args
+  end
+
   local finder = finders.new_async_job({
-    command_generator = function(prompt)
-      if not prompt or prompt == "" then
-        return nil
-      end
-
-      local pieces = vim.split(prompt, "  ")
-      local args = { "rg" }
-      if pieces[1] then
-        table.insert(args, "-e")
-        table.insert(args, pieces[1])
-      end
-
-      if pieces[2] then
-        table.insert(args, "-g")
-        table.insert(args, pieces[2])
-      end
-
-      local flags = {
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-      }
-
-      for _, flag in ipairs(flags) do
-        table.insert(args, flag)
-      end
-
-      return args
-    end,
+    command_generator = command_generator,
     entry_maker = make_entry.gen_from_vimgrep(opts),
     cwd = opts.cwd,
   })
 
   pickers
     .new(opts, {
-      debounce = 100,
+      debounce = 200,
       prompt_title = "Multi Grep",
       finder = finder,
       previewer = conf.grep_previewer(opts),
