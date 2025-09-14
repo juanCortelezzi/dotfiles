295e79d14f1f5e3259b4d55f0512902ea4768eb5
J.B.C <juanbicortelezzi@gmail.com>
2025-09-14 17:29:21 +1200

    feat(nvim): improve telescope grep and actions

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,60 @@
+local live_multigrep = function(opts)
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local make_entry = require("telescope.make_entry")
+  local conf = require("telescope.config").values
+
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job({
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      local flags = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      }
+
+      for _, flag in ipairs(flags) do
+        table.insert(args, flag)
+      end
+
+      return args
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  })
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = "Multi Grep",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require("telescope.sorters").empty(),
+    })
+    :find()
+end
+
 ---@type LazySpec
 return {
   "nvim-telescope/telescope.nvim",
@@ -8,6 +65,20 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<c-t>"] = function(bufnr, opts)
+            require("trouble.sources.telescope").open(bufnr, opts)
+          end,
+        },
+        n = {
+          ["<c-t>"] = function(bufnr, opts)
+            require("trouble.sources.telescope").open(bufnr, opts)
+          end,
+        },
+      },
+    },
     extensions = {
       fzf = {},
     },
@@ -28,7 +99,7 @@ return {
     {
       "<leader>g",
       function()
-        require("telescope.builtin").live_grep()
+        live_multigrep()
       end,
       desc = "Telescope live grep",
     },
